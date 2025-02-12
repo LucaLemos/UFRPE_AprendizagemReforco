0cee4779ce43373f412d27a3fa6df72c26f0f7a9
LuizMFL <luiz.m.franca1@gmail.com>
2025-02-12 17:55:37 -0300

    [fix] Expected-SARSA limited by steps

--- util/algorithms.py
+++ util/algorithms.py
@@ -1,8 +1,8 @@
-from qtable_helper import epsilon_greedy, epsilon_greedy_probs
+from .qtable_helper import epsilon_greedy, epsilon_greedy_probs
 import numpy as np
 
 # Algoritmo Expected-SARSA
-def run_expected_sarsa(env, episodes, lr=0.1, gamma=0.95, epsilon=0.1, verbose=True):
+def run_expected_sarsa(env, steps, lr=0.1, gamma=0.95, epsilon=0.1, verbose=True):
     num_actions = env.action_space.n
 
     # inicializa a tabela Q toda com zeros
@@ -12,45 +12,43 @@ def run_expected_sarsa(env, episodes, lr=0.1, gamma=0.95, epsilon=0.1, verbose=T
     # para cada episódio, guarda sua soma de recompensas (retorno não-descontado)
     sum_rewards_per_ep = []
     episodes = []
+    transitions = []
+    sum_rewards, reward = 0, 0
+    state, _ = env.reset()
+    done = False
     # loop principal
-    for i in range(episodes):
-        transitions = []
-        done = False
-        sum_rewards, reward = 0, 0
-
-        state, _ = env.reset()
-
-        # executa 1 episódio completo, fazendo atualizações na Q-table
-        while not done:
-            # escolhe a próxima ação -- usa epsilon-greedy
-            action = epsilon_greedy(Q, state, epsilon)
-
-            # realiza a ação, ou seja, dá um passo no ambiente
-            next_state, reward, terminated, trunc, _ = env.step(action)
-            done = terminated or trunc
-
-            if terminated:
-                # para estados terminais
-                V_next_state = 0
-            else:
-                # para estados não-terminais -- valor esperado
-                p_next_actions = epsilon_greedy_probs(Q, next_state, num_actions, epsilon)
-                V_next_state = np.sum( np.asarray(p_next_actions) * Q[next_state] )
-
-            # atualiza a Q-table
-            # delta = (estimativa usando a nova recompensa) - estimativa antiga
-            delta = (reward + gamma * V_next_state) - Q[state,action]
-            Q[state,action] = Q[state,action] + lr * delta
-
-            transitions.append((state, action, reward, next_state, done))
-            sum_rewards += reward
-            state = next_state
-
-        # salva o retorno do episódio que encerrou
+    for i in range(steps):
+        # escolhe a próxima ação -- usa epsilon-greedy
+        action = epsilon_greedy(Q, state, epsilon)
+        # realiza a ação, ou seja, dá um passo no ambiente
+        next_state, reward, terminated, trunc, _ = env.step(action)
+        done = terminated or trunc
+        if terminated:
+            # para estados terminais
+            V_next_state = 0
+        else:
+            # para estados não-terminais -- valor esperado
+            p_next_actions = epsilon_greedy_probs(Q, next_state, num_actions, epsilon)
+            V_next_state = np.sum( np.asarray(p_next_actions) * Q[next_state] )
+        # atualiza a Q-table
+        # delta = (estimativa usando a nova recompensa) - estimativa antiga
+        delta = (reward + gamma * V_next_state) - Q[state,action]
+        Q[state,action] = Q[state,action] + lr * delta
+        transitions.append((state, action, reward, next_state, done))
+        sum_rewards += reward
+        state = next_state
+        if done:
+            # salva o retorno do episódio que encerrou
+            sum_rewards_per_ep.append(sum_rewards)
+            episodes.append(transitions)
+            # reseta o ambiente para o novo episódio
+            transitions = []
+            sum_rewards, reward = 0, 0
+            state, _ = env.reset()
+            done = False
+    # Se o episódio foi interrompido antes de ser salvo, adicionamos os dados pendentes
+    if transitions:
         sum_rewards_per_ep.append(sum_rewards)
         episodes.append(transitions)
-        # a cada 100 episódios, imprime informação sobre o progresso
-        if verbose and ((i+1) % 100 == 0):
-            avg_reward = np.mean(sum_rewards_per_ep[-100:])
-            print(f"Episode {i+1} Average Reward (last 100): {avg_reward:.3f}")
+    
     return sum_rewards_per_ep, Q, episodes
