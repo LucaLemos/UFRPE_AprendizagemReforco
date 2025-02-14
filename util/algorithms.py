05115d66de3942f8028166281c01a52e3900d3cb
LuizMFL <luiz.m.franca1@gmail.com>
2025-02-14 15:08:29 -0300

    [fix] Expected-SARSA Correct

--- util/algorithms.py
+++ util/algorithms.py
@@ -1,8 +1,9 @@
 from .qtable_helper import epsilon_greedy, epsilon_greedy_probs
 import numpy as np
+from tianshou.data import ReplayBuffer, Batch
 
 # Algoritmo Expected-SARSA
-def run_expected_sarsa(replay_buffer, env, steps, lr=0.1, gamma=0.95, epsilon=0.1, verbose=True):
+def run_expected_sarsa(env, steps, lr=0.1, gamma=0.95, epsilon=0.1, verbose=True):
     print(f"Run Expected_sar in {env}")
     
     num_actions = env.action_space.n
@@ -17,12 +18,16 @@ def run_expected_sarsa(replay_buffer, env, steps, lr=0.1, gamma=0.95, epsilon=0.
     state, _ = env.reset()
     done = False
     count_ep = 0
+    
+    # ReplayBuffer para armazenar as transições
+    replay_buffer = ReplayBuffer(size=steps)
+
     # loop principal
     for i in range(steps):
         # escolhe a próxima ação -- usa epsilon-greedy
         action = epsilon_greedy(Q, state, epsilon)
         # realiza a ação, ou seja, dá um passo no ambiente
-        next_state, reward, terminated, trunc, _ = env.step(action)
+        next_state, reward, terminated, trunc, info = env.step(action)
         done = terminated or trunc
         if terminated:
             # para estados terminais
@@ -35,16 +40,30 @@ def run_expected_sarsa(replay_buffer, env, steps, lr=0.1, gamma=0.95, epsilon=0.
         # delta = (estimativa usando a nova recompensa) - estimativa antiga
         delta = (reward + gamma * V_next_state) - Q[state,action]
         Q[state,action] = Q[state,action] + lr * delta
-        replay_buffer.append((state, action, reward, next_state, done))
+
+        replay_buffer.add(
+            Batch(
+                obs=state,
+                act=action,
+                rew=reward,
+                obs_next=next_state,
+                terminated=terminated,
+                truncated=trunc,
+                done=done,
+                info=info
+            )
+        )
+        
         sum_rewards += reward
         state = next_state
-        if done:
+        if done or i == steps - 1:
             # salva o retorno do episódio que encerrou
             sum_rewards_per_ep.append(sum_rewards)
-            if verbose and count_ep % 10 == 0:
-                print(f"Episódio {count_ep} terminou com recompensa {sum_rewards} na transição {i}")
+            if verbose and count_ep % 100 == 0:
+                print(f"Episódio {count_ep} {"NÃO terminou e teve" if i == steps - 1 else "terminou com"} recompensa {sum_rewards} na transição {i}")
             # reseta o ambiente para o novo episódio
             sum_rewards, reward = 0, 0
             state, _ = env.reset()
             done = False
+            count_ep += 1
     return sum_rewards_per_ep, Q, replay_buffer
